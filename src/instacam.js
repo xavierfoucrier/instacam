27928404a4b37444ac0b795aed975077cfd2159b
Xavier Foucrier <xavier.foucrier@gmail.com>
2018-08-07 12:15:40 +0200

    Base method structure

--- src/instacam.js
+++ src/instacam.js
@@ -2,4 +2,18 @@
 
 export default class Instacam {
 
+	// class constructor
+	constructor() {}
+
+	// captures the media stream to the viewport through getUserMedia API
+	capture() {}
+
+	// applies a custom filter to the viewport
+	filter() {}
+
+	// snaps the viewport and returns the image data
+	snap() {}
+
+	// saves the viewport to a specific image file format : png and high quality by default
+	save() {}
 }
