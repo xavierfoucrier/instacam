3f1aa56c699e205a59cad42c8e4b9a4ffbb56443
Xavier Foucrier <xavier.foucrier@gmail.com>
2018-08-21 00:03:57 +0200

    Webpack configurations
    Defines the webpack development and production configurations.

---
+++ webpack.prod.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const uglify = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  extends: path.resolve(__dirname, 'webpack.dev.js'),
+  mode: 'production',
+  watch: false,
+  output: {
+    filename: 'instacam.min.js'
+  },
+  optimization: {
+    minimizer: [
+      new uglify({
+        parallel: true
+      })
+    ]
+  }
+};
