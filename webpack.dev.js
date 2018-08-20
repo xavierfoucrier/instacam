f958368cb8592050cd3323146db4c887b95f8d1e
Xavier Foucrier <xavier.foucrier@gmail.com>
2018-08-21 00:03:57 +0200

    Webpack configurations
    Defines the webpack development and production configurations.

---
+++ webpack.dev.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  watch: true,
+  entry: './src/instacam.js',
+  output: {
+    filename: 'instacam.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
