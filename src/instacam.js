fa319e04ac471b7c3e7be4ecb62af8e6c27391cb
Xavier Foucrier <xavier.foucrier@gmail.com>
2018-08-23 16:38:01 +0200

    Viewport element

--- src/instacam.js
+++ src/instacam.js
@@ -5,10 +5,20 @@ import {defaults} from './defaults.js';
 export class Instacam {
 
   // class constructor
-  constructor(options) {
+  constructor(viewport, options) {
+
+    // checks the viewport element
+    if (typeof viewport === 'undefined' || viewport === null || viewport.nodeName.toLowerCase() !== 'canvas') {
+      throw new Error('Invalid viewport, you need to pass a valid HTML5 canvas element');
+    }
 
     // assigns custom user options to defaults
     this.options = Object.assign(defaults, options);
+
+    // initializes the viewport
+    this.viewport = viewport;
+    this.viewport.width = this.options.width;
+    this.viewport.height = this.options.height;
   }
 
   // captures the media stream to the viewport through getUserMedia API
