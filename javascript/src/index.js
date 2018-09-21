1c188c80b445195d7820622d361b1527afb7780d
Xavier Foucrier <xavier.foucrier@gmail.com>
2018-09-21 13:53:05 +0200

    Binds Instacam to css filter inputs

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -3,10 +3,14 @@
 // uses pseudo node module for now as Instacam is not published yet on the NPM registry
 import {Instacam} from '../../node_modules/instacam/src/instacam.js';
 
-{
+// instantiate the class
+let camera = new Instacam(
+  document.querySelector('canvas')
+);
 
-  // instantiate the class
-  let camera = new Instacam(
-    document.querySelector('canvas')
-  );
-}
+// binds all css filter input to properly update the viewport
+Array.from(document.querySelectorAll('.css-filter [type="range"]')).forEach(function(element) {
+  element.addEventListener('input', function() {
+    camera[this.name] = Number.parseFloat(this.value);
+  });
+});
