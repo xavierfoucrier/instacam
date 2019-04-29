f4d9deb1bdd6e838e01972527b0f2a1ff2c7b29c
Xavier Foucrier <xavier.foucrier@gmail.com>
2019-04-29 14:59:10 +0200

    Use the Git imperative convention
    Code comments and commits are now using the Git imperative syntax convention.

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-// imports the library
+// import the library
 import { Instacam } from 'instacam';
 
-// gets some elements
+// get some elements
 let log = document.querySelector('.log');
 let preview = document.querySelector('.preview');
 
@@ -24,19 +24,19 @@ let camera = new Instacam(
   }
 );
 
-// binds all property input to properly update the viewport
+// bind all property input to properly update the viewport
 Array.from(document.querySelectorAll('.property + [type="range"]')).forEach((element) => {
   const value = element.parentNode.querySelector('.value');
 
-  // defines data default attribute automatically
+  // define data default attribute automatically
   element.setAttribute('data-default', element.value);
 
-  // updates the current range value and the associated Instacam property when input is changing
+  // update the current range value and the associated Instacam property when input is changing
   element.addEventListener('input', () => {
     value.innerHTML = element.value;
     camera[element.name] = Number.parseFloat(element.value);
 
-    // updates the volume icon to represent the volume state
+    // update the volume icon to represent the volume state
     if (element.name === 'volume') {
       const state = document.querySelector('.volume-state use');
 
@@ -50,23 +50,23 @@ Array.from(document.querySelectorAll('.property + [type="range"]')).forEach((ele
     }
   });
 
-  // shows the current range value on mousedown
+  // show the current range value on mousedown
   element.addEventListener('mousedown', () => {
     value.innerHTML = element.value;
     value.classList.add('update');
   });
 
-  // hides the current range value on mouseup
+  // hide the current range value on mouseup
   element.addEventListener('mouseup', () => {
     value.classList.remove('update');
   });
 });
 
-// binds all custom blend input to properly update the viewport
+// bind all custom blend input to properly update the viewport
 Array.from(document.querySelectorAll('[name="blend"]')).forEach((element) => {
   element.addEventListener('change', () => {
 
-    // applies the custom blend layer
+    // apply the custom blend layer
     camera.blend = element.value === 'none' ? {} : {
       mode: element.value,
       color: '#217fcf'
@@ -74,11 +74,11 @@ Array.from(document.querySelectorAll('[name="blend"]')).forEach((element) => {
   });
 });
 
-// binds all custom filter input to properly update the viewport
+// bind all custom filter input to properly update the viewport
 Array.from(document.querySelectorAll('[name="filter"]')).forEach((element) => {
   element.addEventListener('change', () => {
 
-    // defines all custom filters
+    // define all custom filters
     const filters = {
       'none': null,
       'noise': (pixel) => {
@@ -113,71 +113,71 @@ Array.from(document.querySelectorAll('[name="filter"]')).forEach((element) => {
       }
     };
 
-    // applies the custom filter
+    // apply the custom filter
     camera.filter = filters[element.value];
   });
 });
 
-// applies the mirror mode to the viewport
+// apply the mirror mode to the viewport
 Array.from(document.querySelectorAll('[name="mirror"]')).forEach((element) => {
   element.addEventListener('change', () => {
     camera.mirror = element.value === '1';
   });
 });
 
-// snaps the viewport and displays a thumbnail
+// snap the viewport and display a thumbnail
 document.querySelector('[name="snap"]').addEventListener('click', () => {
 
-  // snaps the camera
+  // snap the camera
   let data = camera.snap();
 
-  // creates a canvas to paste the snapshot
+  // create a canvas to paste the snapshot
   let canvas = document.createElement('canvas');
   canvas.setAttribute('width', 400);
   canvas.setAttribute('height', 300);
   canvas.getContext('2d').putImageData(data, 0, 0);
 
-  // cleans the area before displaying thumbnail
+  // clean the area before displaying thumbnail
   if (preview.querySelector('img') !== null || (preview.querySelectorAll('canvas').length >= 16)) {
     preview.innerHTML = '';
   }
 
-  // appends the snapshot into the export area
+  // append the snapshot into the export area
   preview.appendChild(canvas);
 });
 
-// saves the viewport when the exported format is changing
+// save the viewport when the exported format is changing
 Array.from(document.querySelectorAll('[name="format"]')).forEach((element) => {
   element.addEventListener('change', () => {
     save();
   });
 });
 
-// saves the viewport when the exported quality is changing
+// save the viewport when the exported quality is changing
 document.querySelector('[name="quality"]').addEventListener('input', () => {
   save();
 });
 
-// saves the viewport and displays the exported image data
+// save the viewport and display the exported image data
 function save() {
 
-  // activates the quality input
+  // activate the quality input
   document.querySelector('[name="quality"]').removeAttribute('disabled');
   document.querySelector('.size').classList.remove('disabled');
 
-  // gets the image data
+  // get the image data
   let data = camera.save(document.querySelector('[name="format"]:checked').value, Number.parseFloat(document.querySelector('[name="quality"]').value));
 
-  // creates the exported image in the specified format and quality
+  // create the exported image in the specified format and quality
   let image = document.createElement('img');
   image.setAttribute('width', 400);
   image.setAttribute('height', 300);
   image.setAttribute('src', data);
 
-  // appends the image into the preview area
+  // append the image into the preview area
   preview.innerHTML = '';
   preview.appendChild(image);
 
-  // estimates the file size
+  // estimate the file size
   document.querySelector('.size span').innerHTML = Math.round(((data.length * 3) / 4) / 1024);
 }
