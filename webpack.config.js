524e5958899288d30623edae8b88037d5e671271
Xavier Foucrier <xavier.foucrier@gmail.com>
2019-11-04 23:41:31 +0100

    Upgrade the webpack configuration to build both JS and CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 'use strict';
 
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   mode: 'production',
-  entry: './javascript/src/index.js',
+  entry: [
+    './javascript/src/index.js',
+    './style/src/build.less'
+  ],
   output: {
     filename: 'app.min.js',
     path: path.resolve(__dirname, 'javascript')
@@ -21,5 +26,44 @@ module.exports = {
         }
       })
     ]
-  }
+  },
+  module: {
+    rules: [{
+      test: /\.(less|css)$/,
+      use: [
+        MiniCssExtractPlugin.loader, {
+          loader: 'css-loader',
+          options: {
+            url: false
+          }
+        }, {
+          loader: 'less-loader',
+          options: {
+            relativeUrls: false
+          }
+        }
+      ]
+    }, {
+      test: /\.(scss)$/,
+      use: [
+        {
+          loader: 'sass-loader'
+        }
+      ]
+    }]
+  },
+  plugins: [
+    new OptimizeCSSAssetsPlugin({
+      cssProcessorPluginOptions: {
+        preset: ['default', {
+          discardComments: {
+            removeAll: true
+          }
+        }],
+      }
+    }),
+    new MiniCssExtractPlugin({
+      filename: '../style/default.min.css'
+    })
+  ]
 };
